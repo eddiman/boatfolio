a20144341735a0ac3d3c909c4df3ad734009bfd8
eddiman <edbjorgen@gmail.com>
2020-08-05 22:05:36 +0200

    added mouse controls and WASD

--- Assets/Scripts/SimpleBoatController.cs
+++ Assets/Scripts/SimpleBoatController.cs
@@ -1,77 +1,107 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SimpleBoatController : MonoBehaviour
 {
-  #region Inspector
+    #region Inspector
 
-  [SerializeField]
-  private Rigidbody physics = default;
+    [SerializeField]
+    private Rigidbody physics = default;
 
-  [SerializeField]
-  [Min(1)]
-  private float speed = default;
+    [SerializeField]
+    [Min(1)]
+    private float speed = default;
 
-  [SerializeField]
-  private ParticleSystem smokeParticles = default;
+    [SerializeField]
+    private ParticleSystem smokeParticles = default;
 
-  #endregion
-
-
-  #region Fields
-
-  private float _force;
-
-  private float _direction;
-
-  #endregion
+    public Vector3 worldPosition;
+    #endregion
 
 
-  #region MonoBehaviour
+    #region Fields
 
-  private void Update ()
-  {
-    // Force input
-    float desiredForce = 0;
-    if ( Input.GetKey(KeyCode.UpArrow) )
-      desiredForce += 1;
-    if ( Input.GetKey(KeyCode.DownArrow) )
-      desiredForce -= 1;
+    private float _force;
 
-    // Direction input
-    float desiredDirection = 0;
-    if ( Input.GetKey(KeyCode.RightArrow) )
-      desiredDirection += 1;
-    if ( Input.GetKey(KeyCode.LeftArrow) )
-      desiredDirection -= 1;
+    private float _direction;
 
-    // Acceleration
-    _force = Mathf.SmoothStep(_force, desiredForce, Time.deltaTime * 10);
-    _direction =
-      Mathf.SmoothStep(_direction, desiredDirection, Time.deltaTime * 4);
-  }
+    #endregion
 
-  private void FixedUpdate ()
-  {
-    // Velocity relative to local space
-    Vector3 velocity = new Vector3(0, 0, _force * speed);
-    physics.velocity = physics.transform.TransformDirection(velocity);
 
-    // Torque
-    Vector3 torque = new Vector3(0, _direction * speed, 0);
-    physics.angularVelocity = torque;
-  }
+    #region MonoBehaviour
 
-  private void OnTriggerEnter (Collider other)
-  {
-    if ( other.CompareTag("Bridge") )
-      smokeParticles.Stop();
-  }
+    private void Update ()
+    {
 
-  private void OnTriggerExit (Collider other)
-  {
-    if ( other.CompareTag("Bridge") )
-      smokeParticles.Play();
-  }
+        float desiredForce = 0;
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, 0);
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
+            {
+                worldPosition = ray.GetPoint(distance);
+            }
+            transform.DOLookAt(worldPosition, 2);
 
-  #endregion
+            desiredForce += 1;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Plane plane = new Plane(Vector3.up, 0);
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
+            {
+                worldPosition = ray.GetPoint(distance);
+            }
+            transform.DOLookAt(worldPosition, 2);
+
+            desiredForce -= 1;
+        }
+
+        // Force input
+        if ( Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) )
+            desiredForce += 1;
+        if ( Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) )
+            desiredForce -= 1;
+
+        // Direction input
+        float desiredDirection = 0;
+        if ( Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.D) )
+            desiredDirection += 1;
+        if ( Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) )
+            desiredDirection -= 1;
+
+        // Acceleration
+        _force = Mathf.SmoothStep(_force, desiredForce, Time.deltaTime * 10);
+        _direction =
+            Mathf.SmoothStep(_direction, desiredDirection, Time.deltaTime * 4);
+    }
+
+    private void FixedUpdate ()
+    {
+        // Velocity relative to local space
+        Vector3 velocity = new Vector3(0, 0, _force * speed);
+        physics.velocity = physics.transform.TransformDirection(velocity);
+
+        // Torque
+        Vector3 torque = new Vector3(0, _direction * speed, 0);
+        physics.angularVelocity = torque;
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if ( other.CompareTag("Bridge") )
+            smokeParticles.Stop();
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        if ( other.CompareTag("Bridge") )
+            smokeParticles.Play();
+    }
+
+    #endregion
 }
